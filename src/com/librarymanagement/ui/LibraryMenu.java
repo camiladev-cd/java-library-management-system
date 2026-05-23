59dc81ce406b94293cbe13f4eda273a8543f352d
camiladev-cd <duenas1521@gmail.com>
2026-05-22 19:14:51 -0500

    Actualizacion Library Menu

--- src/com/librarymanagement/ui/LibraryMenu.java
+++ src/com/librarymanagement/ui/LibraryMenu.java
@@ -1,9 +1,10 @@
 package com.librarymanagement.ui;
 
+import com.librarymanagement.enums.BookCategory;
 import com.librarymanagement.model.Book;
-import com.librarymanagement.service.MemberService;
 import com.librarymanagement.service.BookService;
 import com.librarymanagement.service.LoanService;
+import com.librarymanagement.service.MemberService;
 
 import java.util.Scanner;
 
@@ -13,25 +14,29 @@ public class LibraryMenu {
     private MemberService memberService;
     private LoanService loanService;
 
+    private Scanner scanner;
+    private int option;
+
     public LibraryMenu() {
         this.bookService = new BookService();
         this.memberService = new MemberService();
         this.loanService = new LoanService();
+
+        this.scanner = new Scanner(System.in);
     }
 
-    Scanner scanner = new Scanner(System.in);
-    int option = 0;
-
-    public void start (){
+    public void start() {
 
         do {
+
             System.out.println("\n--- GESTOR DE MANEJO DE LIBROS ---");
             System.out.println("1. Registrar libro");
             System.out.println("2. Registrar miembro");
-            System.out.println("3. Solicitar prestamo libro");
+            System.out.println("3. Solicitar préstamo libro");
             System.out.println("4. Devolver libro");
             System.out.println("5. Lista de libros");
             System.out.println("6. Salir");
+
             System.out.print("Elija una opción: ");
 
             if (scanner.hasNextInt()) {
@@ -44,17 +49,33 @@ public class LibraryMenu {
             }
 
             if (option == 1) {
-                System.out.print("Ingrese Titulo del libro: ");
+
+                System.out.print("Título: ");
                 String title = scanner.nextLine();
-                System.out.print("Ingrese Autor del libro: ");
+
+                System.out.print("Autor: ");
                 String author = scanner.nextLine();
-                System.out.print("Ingrese Categoria del libro: ");
-                String category = scanner.nextLine();
+
+                System.out.print("Categoría: ");
                 String texto = scanner.nextLine().toUpperCase();
 
+                BookCategory catValue = BookCategory.valueOf(texto);
 
-                Book newBook = new Book(title, author, catvalue);
+                Book newBook = new Book(
+                        title,
+                        author,
+                        catValue
+                );
+
+                bookService.registerBook(newBook);
+
+                System.out.println("Libro registrado correctamente.");
             }
-        }
+
+        } while (option != 6);
+
+
+        System.out.print("Ingrese nombre del miembro: ");
+
     }
 }
